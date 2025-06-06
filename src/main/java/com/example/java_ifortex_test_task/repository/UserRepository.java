9db4c0ad1ab5855ae7d8bfb3ba5de634c719fc58
ilumrhisoka <baabaabooooey@gmail.com>
2025-06-06 09:06:58 +0300

    Implement required SQL queries and service methods

--- src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "", nativeQuery = true)
-    List<User> getUsersWithAtLeastOneMobileSession(DeviceType deviceType);
+    @Query(value = "SELECT u.* FROM \"user\" u JOIN session s ON u.id = s.user_id WHERE s.device_type = 'MOBILE' GROUP BY u.id ORDER BY MAX(s.started_at_utc) DESC", nativeQuery = true)
+    List<User> getUsersWithAtLeastOneMobileSession();
 
-    @Query(value = "", nativeQuery = true)
+    @Query(value = "SELECT u.* FROM \"user\" u JOIN session s ON u.id = s.user_id GROUP BY u.id ORDER BY COUNT(s.id) DESC LIMIT 1", nativeQuery = true)
     User getUserWithMostSessions();
 }
--- src/main/java/com/example/java_ifortex_test_task/service/SessionService.java
+++ src/main/java/com/example/java_ifortex_test_task/service/SessionService.java
@@ -1,12 +1,16 @@
 package com.example.java_ifortex_test_task.service;
 
 import com.example.java_ifortex_test_task.dto.SessionResponseDTO;
+import com.example.java_ifortex_test_task.entity.DeviceType;
+import com.example.java_ifortex_test_task.entity.Session;
 import com.example.java_ifortex_test_task.mapper.SessionMapper;
 import com.example.java_ifortex_test_task.repository.SessionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +19,23 @@ public class SessionService {
     private final SessionMapper sessionMapper;
 
     // Returns the first (earliest) desktop Session
-    public SessionResponseDTO getFirstDesktopSession() {
-        return null;
+    public SessionResponseDTO getFirstDesktopSession(DeviceType deviceType) {
+        Session session = sessionRepository.getFirstDesktopSession(deviceType);
+        if (session == null) {
+            return null;
+        }
+        return sessionMapper.toDto(session);
     }
+
 
     // Returns only Sessions from Active users that were ended before 2025
     public List<SessionResponseDTO> getSessionsFromActiveUsersEndedBefore2025() {
-        return null;
+        LocalDateTime endDate = LocalDateTime.of(2025, 1, 1, 0, 0, 0);
+        List<Session> sessions = sessionRepository.getSessionsFromActiveUsersEndedBefore2025(endDate);
+
+        // Преобразуем список сущностей Session в список SessionResponseDTO
+        return sessions.stream()
+                .map(sessionMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
